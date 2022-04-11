a70da8c855b2386ea1d295a70527973ffa06eaaa
volkan karaali <volkankaraali1@gmail.com>
2022-04-11 16:37:56 +0300

    responsive css added

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import AnswerCircle from '../constants/icon/answerCircle'
+import useWindowSize from '../hooks/useWindowSize';
 
 function Answers({ answers, isClick, answerIsCorrect, handleAnswer }) {
 
     const [newAnswerArr, setNewAnswerArr] = useState([])
     const [clickedButton, setClickedButton] = useState(false)
+
+    const [width,] = useWindowSize();
+
 
     useEffect(() => {
         //random sort in array
@@ -16,17 +20,17 @@ function Answers({ answers, isClick, answerIsCorrect, handleAnswer }) {
         <div className='answers'>
             <div className='firstAnswer' >
                 <button className='answerContainer' disabled={isClick && true} onClick={() => { handleAnswer(newAnswerArr[0]); setClickedButton(true) }}>
-                    <AnswerCircle data={newAnswerArr[0]?.answer} color={clickedButton && newAnswerArr[0]?.isTrue && 'black'} />
+                    <AnswerCircle data={newAnswerArr[0]?.answer} width={width === 1440 ? "150" : width === 1024 ? "130" : ""} color={clickedButton && newAnswerArr[0]?.isTrue && 'black'} />
                 </button>
             </div>
             <div className='secondAnswer' >
                 <button className="answerContainer" disabled={isClick && true} onClick={() => { handleAnswer(newAnswerArr[1]); setClickedButton(true) }}>
-                    <AnswerCircle data={newAnswerArr[1]?.answer} color={clickedButton && newAnswerArr[1]?.isTrue && 'black'} />
+                    <AnswerCircle data={newAnswerArr[1]?.answer} width={width === 1440 ? "150" : width === 1024 ? "130" : ""} color={clickedButton && newAnswerArr[1]?.isTrue && 'black'} />
                 </button>
             </div>
             <div className='thirdAnswer' >
                 <button className="answerContainer" disabled={isClick && true} onClick={() => { handleAnswer(newAnswerArr[2]); setClickedButton(true) }}>
-                    <AnswerCircle data={newAnswerArr[2]?.answer} color={clickedButton && newAnswerArr[2]?.isTrue && 'black'} />
+                    <AnswerCircle data={newAnswerArr[2]?.answer} width={width === 1440 ? "150" : width === 1024 ? "130" : ""} color={clickedButton && newAnswerArr[2]?.isTrue && 'black'} />
                 </button>
             </div>
         </div>
